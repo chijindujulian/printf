2d140e21eccb1d5e130d82f07cfb84c4912660b8
chijindu julian <juliancrosss@gmail.com>
2024-01-30 17:46:20 +0100

    edit _printf.c

--- _printf.c
+++ _printf.c
@@ -50,29 +50,3 @@ int _printf(char *format, ...)
     va_end(args);
     return (counter);
 }
-
-/**
- * main - entry point of the program
- *
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-    printf("Hello world\n");
-    printf("A character %c\n", 'j');
-    printf("A string %s\n", "hello");
-    printf("An integer %d\n", 12);
-    printf("A negative integer %d\n\n", -12);
-
-    printf("A line\n\n");
-
-    _printf("Hello world\n");
-    _printf("A character %c\n", 'j');
-    _printf("A string %s\n", "hello");
-    _printf("An integer %d\n", 12);
-    _printf("A negative integer %d\n", -12);
-
-    return (0);
-}
