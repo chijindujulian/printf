014eeddf9ec12fc1f4b67bcc8d56752028aa43f9
chijindu julian <juliancrosss@gmail.com>
2024-01-30 14:09:40 +0100

    task 2

--- _printf.c
+++ _printf.c
@@ -10,45 +10,57 @@
 
 int _printf(char *format, ...)
 {
-	int counter = 0;
+    int counter = 0;
 
-	va_list args;
+    va_list args;
 
-	va_start(args, format);
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				char s = va_arg(args, int);
+    va_start(args, format);
 
-				_putchar(s);
-				counter++;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int i = va_arg(args, int);
+    if (format == NULL)
+    {
+        return (-1);
+    }
+    
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char s = va_arg(args, int);
 
-				counter += _print_int(i);
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
+                _putchar(s);
+                counter++;
+            }
+            else if (*format == 'd' || *format == 'i')
+            {
+                int i = va_arg(args, int);
 
-				counter += _print_string(str);
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			counter++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (counter);
+                counter += _print_int(i);
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+
+                counter += _print_string(str);
+            }
+            else
+            {
+                _putchar('%');
+				_putchar(*format);
+				counter += 2;
+            }
+        }
+        else
+        {
+            _putchar(*format);
+            counter++;
+        }
+        format++;
+    }
+    va_end(args);
+    return (counter);
 }
 
 /**
@@ -60,15 +72,16 @@ int _printf(char *format, ...)
 
 int main(void)
 {
-	printf("Wah gwaan\n");
-	printf("An integer man %d\n", -12);
-	printf("A string bredda %s\n", "Bredda");
-	printf("That one ah character bredda %c\n\n", 's');
 
-	_printf("Wah gwaan\n");
-	_printf("An integer man %d\n", -32);
-	_printf("A string bredda %s\n", "Bredda");
-	_printf("That one ah %c character bredda %c\n", 's', 'f');
+    printf("Wah gwaan\n");
+    printf("An integer man %d\n", -12);
+    printf("A string bredda %s\n", "Bredda");
+    printf("That one ah character bredda %c\n\n", 's');
 
-	return (0);
+    _printf("Wah gwaan\n");
+    _printf("An integer man %d\n", -32);
+    _printf("A string bredda %s\n", "Bredda");
+    _printf("That one ah %c character bredda %c\n", 's', 'f');
+
+    return (0);
 }
